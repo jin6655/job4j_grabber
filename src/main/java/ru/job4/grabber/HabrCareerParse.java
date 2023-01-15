14b8fa9e0ab42574c5724261b252aca6c8510165
jin6655 <vasilypoiser@yandex.ru>
2023-01-15 22:30:59 +0300

    2.3. Загрузка деталей поста.

--- src/main/java/ru/job4/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4/grabber/HabrCareerParse.java
@@ -15,7 +15,20 @@ public class HabrCareerParse {
 
 private static final String SOURCE_LINK = "https://career.habr.com";
 private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
-private static final int NUMBER_PAGES = 5;
+private static final int NUMBER_PAGES = 1;
+
+    private String retrieveDescription(String link) throws IOException {
+        StringBuilder rsl = new StringBuilder();
+        Connection connection = Jsoup.connect(String.format("%s%s", SOURCE_LINK, link));
+        Document document = connection.get();
+        Elements rows = document.select(".basic-section--appearance-vacancy-description");
+        rows.stream().forEach(row -> {
+            Element descriptionTittleElement = row.select(".section-title--divider").first();
+            Element descriptionElement = row.select(".faded-content__container").first();
+            rsl.append(descriptionTittleElement.text()).append("\n").append(descriptionElement.text());
+        });
+        return rsl.toString();
+    }
 
     public static void parse(int n) throws IOException {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
@@ -26,10 +39,17 @@ private static final int NUMBER_PAGES = 5;
             Element titleElement = row.select(".vacancy-card__title").first();
             Element linkElement = titleElement.child(0);
             Element dateElement = row.select(".vacancy-card__date").first().child(0);
+            Element descriptionLinkElement = row.select(".vacancy-card__title").first();
+            String description = "";
+            try {
+                description = new HabrCareerParse().retrieveDescription(descriptionLinkElement.child(0).attr("href"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             String vacancyName = titleElement.text();
             String lInk = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
             LocalDateTime date = new HabrCareerDateTimeParser().parse(dateElement.attr("datetime"));
-            System.out.printf("%s, %s, %s\n", vacancyName, lInk, date.format(formatter));
+            System.out.printf("%s, %s, %s\n%s\n\n", vacancyName, lInk, date.format(formatter), description);
         });
     }
 
--- src/main/java/ru/job4/inst/Caty.java
+++ src/main/java/ru/job4/inst/Caty.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class Caty {
 
-    public static void main(String[] args) {;
+    public static void main(String[] args) {
         System.out.println();
     }
 }
